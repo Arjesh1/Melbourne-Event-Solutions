68161b5766a3b788c83117f0a34aaee417f9cfbf
Arjesh1 <arjesh.khadka2@gmail.com>
2022-07-29 20:47:17 +1000

    initial web

---
+++ script/java.js
@@ -0,0 +1,25 @@
+ /*if (confimr("do you want to continue"))
+{
+	alert ("you select ok:);
+}
+else{
+	alert("you chose to camcel");
+}
+*/
+
+var name= prompt("what is your name?");
+if (name!=null)
+{
+	alert("hello"+ name);
+}
+else {
+alert("you did not tell me your name");
+}
+
+
+
+
+
+
+
+alert("welcome");
